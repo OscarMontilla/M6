cbfa2080627840086e5b3cb4fae303343e20ee46
OscarMontilla <120601904+OscarMontilla@users.noreply.github.com>
2024-01-24 20:52:24 +0100

    Delete historia4 directory

--- historia4/src/main.js
+++
@@ -1,8 +0,0 @@
-import { header } from "./componentes/header.js"
-import { home } from "./vistas/home.js"
-
-
-document.querySelector('header').innerHTML = header.template
-header.script()
-document.querySelector('main').innerHTML = home.template
-home.script()
